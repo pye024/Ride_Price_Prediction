9332fa22e5abfd217cb4ecfac52b60976edf1a4c
peniel <penieljegede@gmail.com>
2025-08-24 10:26:21 +0100

    created table sql

---
+++ query.sql
@@ -0,0 +1,10 @@
+CREATE TABLE RIDE(
+trip_duration decimal,
+distance_traveled decimal,
+num_of_passengers int,
+fare decimal,
+tip decimal,
+miscellaneous_fees decimal,
+total_fare decimal,
+surge_applied int
+);
